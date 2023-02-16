3da790126090b41ce0ad9126630d81856d0abf1e
eunwoo.nam <eunwoo.nam@samsung.com>
2023-02-16 15:33:47 +0900

    lpwork test: reproduce lpwork usagefault, busfault
    Signed-off-by: eunwoo.nam <eunwoo.nam@samsung.com>

--- apps/examples/hello/hello_main.c
+++ apps/examples/hello/hello_main.c
@@ -55,11 +55,50 @@
  ****************************************************************************/
 
 #include <tinyara/config.h>
+#include <tinyara/wqueue.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /****************************************************************************
  * hello_main
  ****************************************************************************/
+
+static clock_t start_time;
+
+struct work_s works[1000];
+struct work_s g_work_test_timer[3];
+
+static void wq_test1(FAR void *arg)
+{
+	clock_t cur_time = 0;
+	cur_time = clock();
+	int count = (int)arg;
+	printf("workqueue_test: count(%d), executed delay is (%llu) ticks.\n", count, (uint64_t)cur_time - (uint64_t)start_time);
+	if(count < 5000){
+		count++;
+		work_queue(LPWORK, &works[count%1000], wq_test1, (void *)count, 0);
+	}
+}
+
+static void work_queue_test_func(FAR void *arg)
+{
+	clock_t cur_time = 0;
+	cur_time = clock();
+	int count = (int)arg;
+	printf("add by other thread: count(%d), executed delay is (%llu) ticks.\n", count, (uint64_t)cur_time - (uint64_t)start_time);
+}
+
+static int func_thread(int argc, char *argv[])
+{
+	int idx = atoi(argv[argc-1]);
+	int tick= idx*10;
+
+	usleep(tick*5000);
+	for(int i= 0; i<5000;i++){
+		int ret;
+		ret = work_queue(LPWORK, &g_work_test_timer[idx-1], work_queue_test_func, i, 0);
+		usleep(tick*1000);
+	}
+}
 
 #ifdef CONFIG_BUILD_KERNEL
 int main(int argc, FAR char *argv[])
@@ -67,6 +106,22 @@ int main(int argc, FAR char *argv[])
 int hello_main(int argc, char *argv[])
 #endif
 {
-	printf("Hello, World!!\n");
+	pid_t pid1;
+	pid_t pid2;
+	pid_t pid3;
+	
+	printf("hello\n");
+
+	start_time = clock();
+	char *const arg1[] = { "1", NULL };
+	pid1 = task_create("wq_test1", 100, 2048, func_thread, (char * const *)arg1);
+	char *const arg2[] = { "2", NULL };
+	pid2 = task_create("wq_test2", 100, 2048, func_thread, (char * const *)arg2);
+	char *const arg3[] = { "3", NULL };
+	pid3 = task_create("wq_test3", 100, 2048, func_thread, (char * const *)arg3);
+
+	work_queue(LPWORK, &works[0], wq_test1, (void *)1, 0);
+	sleep(1000);
 	return 0;
 }
+
